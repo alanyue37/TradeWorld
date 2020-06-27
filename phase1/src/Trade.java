a26709d405411658cdc9a7d734cabaa55de17a88
Alyssa Chan <alyssa.chan@mail.utoronto.com>
2020-06-27 15:46:17 -0500

    Added Abstract Trade class

--- phase1/src/Trade.java
+++ phase1/src/Trade.java
@@ -3,22 +3,65 @@ import java.util.HashMap;
 public abstract class Trade {
     private String type;  //temporary or permanent
     private int numOfEdits;
-    private int id;
-    private String status; //or maybe boolean?
+    private int idOfTrade;
+    private boolean IsCompleted; // transaction is done (permanent: once, temporary: twice)
+    private int numConfirmations; // once they set up the meeting, if they confirmed
+    private static int totalTrade;
     private HashMap meeting; //think of something else better
-    private boolean confirmed;
-    private static int totalTrade; //for id
 
     /**
-     *
      * @param type
      */
-    public Trade(String type){
+    public Trade(String type) {
         this.numOfEdits = 0;
         this.type = type;
-        this.id = totalTrade; //
+        this.idOfTrade = totalTrade + 1;
+        this.IsCompleted = false;
+        this.numConfirmations = 0;
+        totalTrade += 1;
+        this.meeting = new HashMap(); //come back to this
     }
 
+    public String getTradeType() {
+        return this.type;
+    }
+    // ask if we need setter for type of trade
+
+    public HashMap getMeetingInfo() {
+        return meeting;
+    }
+
+    public void setMeetingInfo(HashMap newMeetingInfo) {
+        meeting = newMeetingInfo;
+    }
+
+    public int getNumOfEdits() {
+        return this.numOfEdits;
+    }
+
+    public void incrementNumOfEdits() {
+        this.numOfEdits += 1;
+    }
+
+    public int getIdOfTrade() {
+        return this.idOfTrade;
+    }
+
+    public boolean getIsCompleted() {
+        return IsCompleted;
+    }
+
+    public void setIsCompleted(boolean newIsCompleted) {
+        IsCompleted = newIsCompleted;
+    }
+
+    public int getnumConfirmations() {
+        return numConfirmations;
+    }
+
+    public void incrementnumConfirmations() {
+        numConfirmations++;
+    }
+
+// confirmation of meeting time/place for temporary vs. permanent
 }
-
-
