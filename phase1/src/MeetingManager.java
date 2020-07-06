648b48e5b70533aa31e2ad37033a816f3c0e8a34
zhaoj159 <jiaqiz.zhao@mail.utoronto.ca>
2020-07-06 11:38:30 -0400

    added method to compare numOfEdits with limit

--- phase1/src/MeetingManager.java
+++ phase1/src/MeetingManager.java
@@ -9,10 +9,19 @@ public class MeetingManager {
     }
     // come back to this
 
+    /**
+     * agree with the meeting time, place that the other user suggested (only need one user to confirm since we can
+     * assume that the one suggesting the meeting details agrees.
+     * @param meeting the meeting that the user is agreeing with the time and location.
+     */
     public void confirmAgreement(Meeting meeting) {
         meeting.changeIsConfirmed();
     }
 
+    /**
+     * confirm that meeting happened in real life.
+     * @param meeting meeting that happened in real life
+     */
     public void meetingHappened(Meeting meeting) {
         meeting.incrementNumConfirmations();
 
@@ -21,6 +30,13 @@ public class MeetingManager {
         }
     }
 
+    /**
+     * Return whether a meeting is incomplete. An incomplete meeting is a meeting that the user agreed on the meeting
+     * details (time, location), the meeting time has passed, but at least one of the users have not confirmed the
+     * meeting happened.
+     * @param meeting meeting that is incomplete
+     * @return true if the meeting is incomplete and false if the meeting is completed.
+     */
     public boolean isIncompleteMeeting(Meeting meeting){
         Date today = new Date();
         if (meeting.getIsCompleted()){
@@ -45,6 +61,10 @@ public class MeetingManager {
             return meeting.getTime();
         }
         return null;
+    }
+
+    public boolean attainedThresholdEdits(Meeting meeting, int numOfEditThreshold){
+        return meeting.getNumOfEdits() >= numOfEditThreshold;
     }
 
 
