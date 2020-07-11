489ac807eb37eb062eaefdef0acf9c05c35d9f85
Saumya Bakshi <bakshi.saumya10@gmail.com>
2020-07-11 15:15:01 -0400

    Updated UserController.java and UserPresenter.java to made the code easier to understand (also added Javadoc)

--- phase1/src/UserPresenter.java
+++ phase1/src/UserPresenter.java
@@ -1,25 +1,31 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class UserPresenter {
     TradeModel tradeModel;
 
+    /**
+     * Constructor for UserController.
+     * @param tradeModel Model of the system.
+     */
     public UserPresenter(TradeModel tradeModel){
         this.tradeModel = tradeModel;
     }
 
+    /**
+     * Print main menu to run the UserController
+     * There are two options that are available (viewing and trading)
+     */
     public void startMenu() {
         System.out.println("User Options\n" +
-                " Enter 1 to view Inventory\n" +
-                " Enter 2 to view Wishlist\n " +
-                " Enter 3 to chose an item for trading from the Inventory\n " +
-                " Enter 4 to create an item for Trading\n " +
-                " Enter 5 to view look for items to add to your wish list\n" +
-                " Enter 6 to request your account to be unfrozen\n" +
-                " Enter 7 to see the last three trades\n" +
-                " Enter 8 to request a trade\n" +
+                " Enter 1 to view \n" +
+                " Enter 2 to trade \n " +
                 "\n Type \"exit\" at any time to exit.");
     }
 
+    /**
+     * Print menu to run the view options for UserController
+     */
     public void printViewOptions() {
         System.out.println("View Options\n" +
                 " Enter 1 to view inventory\n" +
@@ -29,63 +35,77 @@ public class UserPresenter {
                 "\n Type \"exit\" at any time to exit.");
     }
 
+    /**
+     * Print menu to run the trade options for UserController
+     */
     public void printViewTradeOptions() {
         System.out.println("Trade Options\n" +
                 " Enter 1 to lend items\n" +
-                " Enter 2 to borrow items\n " +
-                " Enter 3 to trade items \n " +
+                " Enter 2 to temporarily borrow items\n " +
+                " Enter 3 to permanently borrow items\n " +
+                " Enter 4 to trade items \n " +
                 "\n Type \"exit\" at any time to exit.");
     }
 
-
-    public void printInputUserName(){
-        System.out.println("Enter your name: ");
-    }
-
-    public void printInputUserEmail(){
-        System.out.println("Enter your email: ");
-    }
-
-    public void printInputUserId(){
-        System.out.println("Enter your User Id: ");
-    }
-
+    /**
+     * Print statement to input item name
+     */
     public void printInputItemName(){
         System.out.println("Enter Item's name: ");
     }
 
+    /**
+     * Print statement to input item description
+     */
     public void printInputItemDescription(){
         System.out.println("Enter Item's description: ");
     }
 
-    public void printItemAddingFailure(){
-        System.out.println("Sorry! The item could not be added");
-    }
-
-    public void printUserInventory(String username){
-        System.out.println("Inventory: " + tradeModel.getUserManager().getSetByUsername(username, itemSets.INVENTORY));
-    }
-
-    public void printUserWishlist(String username){
-        System.out.println("Wishlist: " + tradeModel.getUserManager().getSetByUsername(username, itemSets.WISHLIST));
-    }
-
-    public void printSystemInventory(){
-        System.out.println("System Inventory: " + tradeModel.getItemManager().getAvailableItems());
-    }
-
+    /**
+     * Print statement to input item id
+     */
     public void printInputItemID(){
         System.out.println("Enter Item's id: ");
     }
 
-    public void printUserInventoryOptions(){
-        System.out.println("Enter 1 to view Inventory\n or enter \"exit\" \n ");
+    /**
+     * Print the users inventory
+     */
+    public void printUserInventory(String username){
+        System.out.println("Inventory: " + tradeModel.getUserManager().getSetByUsername(username, itemSets.INVENTORY));
     }
 
-    public void printViewTopTradingPartner(ArrayList<String> partners){
+    /**
+     * Print the users wishlist
+     */
+    public void printUserWishlist(String username){
+        System.out.println("Wishlist: " + tradeModel.getUserManager().getSetByUsername(username, itemSets.WISHLIST));
+    }
+
+    /**
+     * Print the systems inventory
+     */
+    public void printSystemInventory(){
+        System.out.println("System Inventory: " + tradeModel.getItemManager().getAvailableItems());
+    }
+
+    /**
+     * Print the options that the users has after viewing the inventory
+     */
+    public void printUserInventoryOptions(){
+        System.out.println("Enter 1 to add something to your wishlist\n or enter \"exit\" \n ");
+    }
+
+    /**
+     * Print the top trading partners of a user
+     */
+    public void printViewTopTradingPartner(List<String> partners){
         System.out.println("Your top 3 trading partners are: " + partners);
     }
 
+    /**
+     * Print the options that a frozen users has
+     */
     public void printAccountIsFrozenOption(){
         System.out.println("Sorry your account is frozen\n" +
                 " Enter 1 to request your account to be unfrozen \n" +
