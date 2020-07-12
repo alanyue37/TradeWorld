5f1b8f29923142cac7338433db45955144ef8415
Jasmine Saini <jamsinemsaini@gmail.com>
2020-07-12 17:02:34 -0400

    Added suggestion to userController run method

--- phase1/src/UserController.java
+++ phase1/src/UserController.java
@@ -33,8 +33,10 @@ public class UserController implements RunnableController {
      */
     public void run() {
         try {
-            if(!selectMenu()) {
-                System.out.println("Please enter a valid input.");
+            UserPresenter.startMenu(); // should this run function have more than 1 while loop (i.e., one for viewing and the other for trade)?
+            String input = br.readLine();
+            while (!input.equals("exit")) {
+                selectMenu();
             }
         } catch (IOException e) {
             System.out.println("Something bad happened.");
