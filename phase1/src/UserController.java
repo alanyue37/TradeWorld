4dad516ce4b4305c777dd008d654dd142d3d9819
Jasmine Saini <jamsinemsaini@gmail.com>
2020-07-12 23:07:32 -0400

    Completed freeze method in AdminController

--- phase1/src/UserController.java
+++ phase1/src/UserController.java
@@ -5,7 +5,6 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
