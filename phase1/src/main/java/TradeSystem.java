386629781f42b9280d09e4478c8b0eec478128ca
James Wang <jamesjwang261@gmail.com>
2020-07-13 18:49:45 -0400

    Reorganized project structure, changed location of serializedobjects.ser

--- phase1/src/main/java/TradeSystem.java
+++ phase1/src/main/java/TradeSystem.java
@@ -1,23 +1,24 @@
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 /**
  * The entire trading system.
  */
 public class TradeSystem {
 
-    private final String serializedDataManagerInfo = "serializedobjects.ser";
+    private final String tradeModelFile = "serializedobjects.ser";
 
     /**
      * Run the trading system.
      */
     public void run() {
         try {
-            DataManager dataManager = new DataManager();
-            TradeModel tradeModel = dataManager.readFromFile(serializedDataManagerInfo);
+            DataManager dataManager = new DataManager(tradeModelFile);
+            TradeModel tradeModel = dataManager.readFromFile();
             LogInController controller = new LogInController(tradeModel);
             RunnableController mainController = controller.getNextController();
             mainController.run(); // This could be either UserController or AdminController
-            dataManager.saveToFile(serializedDataManagerInfo, tradeModel);
+            dataManager.saveToFile(tradeModel);
         } catch (IOException | ClassNotFoundException ex) {
             ex.printStackTrace();
         }
