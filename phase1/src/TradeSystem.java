68ab5c5e4a10ef4b2be0a48d72a8bf2e75f58252
Alyssa Chan <alyssa.chan@mail.utoronto.com>
2020-07-13 19:36:05 -0500

    //

--- phase1/src/TradeSystem.java
+++ phase1/src/TradeSystem.java
@@ -1,12 +1,9 @@
 import java.io.IOException;
-
 /**
  * The entire trading system.
  */
 public class TradeSystem {
-
     String serializedDataManagerInfo = "serializedobjects.ser";
-
     /**
      * Run the trading system.
      */
@@ -14,12 +11,22 @@ public class TradeSystem {
         try {
             DataManager dataManager = new DataManager();
             TradeModel tradeModel = dataManager.readFromFile(serializedDataManagerInfo);
+            // DEMO CODE
+            tradeModel.getUserManager().createAdminUser("a1", "a1", "a1");
+            tradeModel.getUserManager().createAdminUser("a2", "a2", "a2");
+            tradeModel.getUserManager().createTradingUser("u1", "u1", "u1");
+            tradeModel.getUserManager().createTradingUser("u2", "u2", "u2");
+            System.out.println(tradeModel.getUserManager());
             LogInController controller = new LogInController(tradeModel);
             RunnableController mainController = controller.getNextController();
             mainController.run(); // This could be either UserController or AdminController
             dataManager.saveToFile(serializedDataManagerInfo, tradeModel);
         } catch (IOException | ClassNotFoundException ex) {
             ex.printStackTrace();
+        } catch (NullPointerException exception) {
+            // Exit menu option selected
+            // TODO: Create custom exception (e.g. NoNextController)
+            System.out.println("Bye!");
         }
     }
 }
