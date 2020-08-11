5b6d4fce0825d48826d0c5d8ede46bc073ca94e5
Jey <jeykumar3@users.noreply.github.com>
2020-08-11 17:29:37 -0400

    Changes to fix persistence, including override stop method in DemoMain

--- phase2/src/main/java/tradegateway/DemoTradeSystem.java
+++ phase2/src/main/java/tradegateway/DemoTradeSystem.java
@@ -3,8 +3,11 @@ package tradegateway;
 import javafx.stage.Stage;
 import loginadapters.LogInController;
 import loginadapters.LoginGUI;
+import sun.java2d.cmm.Profile;
 import trademisc.RunnableController;
 import trademisc.RunnableGUI;
+import useradapters.LoggedInProfileGUI;
+import useradapters.ProfileGUI;
 
 import java.io.IOException;
 import java.util.Observable;
@@ -31,12 +34,23 @@ public class DemoTradeSystem {
 
             // INITIALIZE TRADEMODEL FOR DEMO
             initializeTradeModel(tradeModel);
+            tradeModel.setCurrentUser("u1");
 
-            gui = new LoginGUI(stage, 275, 300, tradeModel);
+            //gui = new LoginGUI(stage, 275, 300, tradeModel);
+            //gui = new ProfileGUI(stage, 720, 600, tradeModel, "u1");
+            gui = new LoggedInProfileGUI(stage, 720, 600, tradeModel, "u1");
             gui.initialScreen();
-            dataManager.saveToFile(tradeModel);
+
         } catch (IOException | ClassNotFoundException ex) {
             ex.printStackTrace();
+        }
+    }
+
+    public void persist() {
+        try {
+            dataManager.saveToFile(tradeModel);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
