9607e01749c9e0ea0b81183d248998c45716030e
James Wang <jamesjwang261@gmail.com>
2020-08-12 23:28:33 -0400

    LogInPresenter removed, LogInGUI and LogInController cleaned up.

--- phase2/src/main/java/loginadapters/LoginGUI.java
+++ phase2/src/main/java/loginadapters/LoginGUI.java
@@ -29,7 +29,6 @@ public class LoginGUI implements RunnableGUI {
     private final Stage stage;
     private Scene scene;
     private final LogInController controller;
-    private final LogInPresenter presenter;
     private final int width;
     private final int height;
     private final TradeModel model;
@@ -39,23 +38,20 @@ public class LoginGUI implements RunnableGUI {
     public LoginGUI(Stage stage, int width, int height, TradeModel model) {
         this.stage = stage;
         this.controller = new LogInController(model);
-        this.presenter = new LogInPresenter();
         this.width = width;
         this.height = height;
         this.model = model;
     }
 
     public void usernameTaken(String username) {
-        presenter.usernameTaken(username);
-        Text message = new Text(presenter.next());
+        Text message = new Text("Username " + username + " is already taken.");
         if(!grid.getChildren().contains(message)){
             grid.add(message, 0, 6, 2, 1);
         }
     }
 
     public void invalidAccount(){
-        presenter.invalidAccount();
-        Text message = new Text(presenter.next());
+        Text message = new Text("Incorrect username or password.");
         if(!grid.getChildren().contains(message)){
             grid.add(message, 0, 5, 2, 1);
         }
@@ -81,40 +77,20 @@ public class LoginGUI implements RunnableGUI {
     }
 
     private void initializeScreen() {
-        presenter.initialScreen();
-        stage.setTitle(presenter.next());
+        stage.setTitle("Trading System - Login");
 
-        Text title = new Text(presenter.next());
+        Text title = new Text("Welcome");
         title.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
 
-//        Button userLoginBtn = new Button("Log in as a trading user");
-//        Button adminLoginBtn = new Button("Log in as an admin");
-//        Button registerBtn = new Button("Create a new account");
-//        Button demoBtn = new Button("Program demo");
-        presenter.startMenu();
-        List<Button> buttons = new ArrayList<>();
-        while (presenter.hasNext()) {
-            buttons.add(new Button(presenter.next()));
-        }
+        Button userLoginBtn = new Button("Log in as a trading user");
+        Button adminLoginBtn = new Button("Log in as an admin");
+        Button registerBtn = new Button("Create a new account");
+        Button demoBtn = new Button("Program demo");
 
-//        userLoginBtn.setOnAction(actionEvent -> logIn(false));
-//        adminLoginBtn.setOnAction(actionEvent -> logIn(true));
-//        registerBtn.setOnAction(actionEvent -> newAccount());
-//        demoBtn.setOnAction(actionEvent -> {
-//            controller.demo();
-//        });
-        buttons.get(0).setOnAction(actionEvent -> {
-            logIn(false);
-        });
-        buttons.get(1).setOnAction(actionEvent -> {
-            logIn(true);
-        });
-        buttons.get(2).setOnAction(actionEvent -> {
-            newAccount();
-        });
-        buttons.get(3).setOnAction(actionEvent -> {
-            demoUserLogIn();
-        });
+        userLoginBtn.setOnAction(actionEvent -> logIn(false));
+        adminLoginBtn.setOnAction(actionEvent -> logIn(true));
+        registerBtn.setOnAction(actionEvent -> newAccount());
+        demoBtn.setOnAction(actionEvent -> demoUserLogIn());
 
         grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
@@ -122,20 +98,20 @@ public class LoginGUI implements RunnableGUI {
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
 
-//        grid.add(title, 0, 0, 2, 1);
-//        grid.add(userLoginBtn, 0, 1, 2, 1);
-//        grid.add(adminLoginBtn, 0, 2, 2, 1);
-//        grid.add(registerBtn, 0, 3, 2, 1);
-//        grid.add(demoBtn, 0, 4, 2, 1);
         grid.add(title, 0, 0, 2, 1);
-        for (int i = 0; i < buttons.size(); i++) {
-            grid.add(buttons.get(i), 0, i+1, 2, 1);
-        }
+        grid.add(userLoginBtn, 0, 1, 2, 1);
+        grid.add(adminLoginBtn, 0, 2, 2, 1);
+        grid.add(registerBtn, 0, 3, 2, 1);
+        grid.add(demoBtn, 0, 4, 2, 1);
     }
 
     private void logIn(boolean isAdmin){
-        presenter.logIn(isAdmin);
-        Text title = new Text(presenter.next());
+        Text title;
+        if (isAdmin) {
+            title = new Text("Admin login");
+        } else {
+            title = new Text("Trader login");
+        }
 
         title.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
 
@@ -147,12 +123,12 @@ public class LoginGUI implements RunnableGUI {
 
         grid.add(title, 0, 0, 2, 1);
 
-        Label usernameLabel = new Label(presenter.next());
+        Label usernameLabel = new Label("Username:");
         TextField usernameField = new TextField();
-        Label passwordLabel = new Label(presenter.next());
+        Label passwordLabel = new Label("Password:");
         PasswordField passwordField = new PasswordField();
 
-        Button loginButton = new Button(presenter.next());
+        Button loginButton = new Button("Log In");
         HBox hBoxLoginButton = new HBox(10);
         hBoxLoginButton.setAlignment(Pos.BOTTOM_RIGHT);
         hBoxLoginButton.getChildren().add(loginButton);
@@ -186,11 +162,9 @@ public class LoginGUI implements RunnableGUI {
     }
 
     private void newAccount(){
-        presenter.createAccount();
-        Text title = new Text(presenter.next());
+        Text title = new Text("Create a new account");
         title.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
 
-//        GridPane grid = new GridPane();
         grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
@@ -199,16 +173,16 @@ public class LoginGUI implements RunnableGUI {
 
         grid.add(title, 0, 0, 2, 1);
 
-        Label nameLabel = new Label(presenter.next());
+        Label nameLabel = new Label("Name:");
         TextField nameField = new TextField();
-        Label usernameLabel = new Label(presenter.next());
+        Label usernameLabel = new Label("Username:");
         TextField usernameField = new TextField();
-        Label passwordLabel = new Label(presenter.next());
+        Label passwordLabel = new Label("Password:");
         PasswordField passwordField = new PasswordField();
-        Label cityLabel = new Label(presenter.next());
+        Label cityLabel = new Label("City:");
         TextField cityField = new TextField();
 
-        Button registerButton = new Button(presenter.next());
+        Button registerButton = new Button("Register");
         HBox hBoxRegisterButton = new HBox(10);
         hBoxRegisterButton.setAlignment(Pos.BOTTOM_RIGHT);
         hBoxRegisterButton.getChildren().add(registerButton);
@@ -284,16 +258,14 @@ public class LoginGUI implements RunnableGUI {
     }
 
     public void tryAgain() {
-        presenter.tryAgain();
-        Text message = new Text(presenter.next());
+        Text message = new Text("Please try again");
         if (!grid.getChildren().contains(message)) {
             grid.add(message, 0, 10, 1, 1);
         }
     }
 
     private void backButton() {
-        presenter.backButton();
-        Button backButton = new Button(presenter.next());
+        Button backButton = new Button("Back");
         grid.add(backButton, 0, 6);
         backButton.setOnAction(actionEvent -> {
             initialScreen();
